10ce542eee680e332d59592f013fd6513cb2ce37
unknown <juanchicanestrari@gmail.com>
2023-10-18 16:34:41 -0300

    INNITAL COMMIT BACK END

---
+++ backEnd/src/test/java/seminario/grupo4/smart_travel/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package seminario.grupo4.smart_travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
