af8911031677a7aa5fcc86ed9cbdb9091c163b10
unknown <juanchicanestrari@gmail.com>
2023-11-06 17:18:40 -0300

    FEAT (DTOs) Agregamos los dtos para Destino y Actividades

--- backEnd/src/main/java/seminario/grupo4/smart_travel/model/dto/ActividadesDTO.java
+++ backEnd/src/main/java/seminario/grupo4/smart_travel/model/dto/ActividadesDTO.java
@@ -1,2 +1,33 @@
-package seminario.grupo4.smart_travel.model.dto;public class ActividadesDTO {
+package seminario.grupo4.smart_travel.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ActividadesDTO {
+    private String nombreActividad;
+    private String lugar;
+    private String fecha;
+    private Long viajeId;
+
+    public ActividadesDTO() {
+    }
+
+    public ActividadesDTO(String nombreActividad, String lugar, String fecha, Long viajeId) {
+        this.nombreActividad = nombreActividad;
+        this.lugar = lugar;
+        this.fecha = fecha;
+        this.viajeId = viajeId;
+    }
+
+    @Override
+    public String toString() {
+        return "ActividadesDTO{" +
+                "nombreActividad='" + nombreActividad + '\'' +
+                ", lugar='" + lugar + '\'' +
+                ", fecha='" + fecha + '\'' +
+                ", viajeId=" + viajeId +
+                '}';
+    }
 }
--- backEnd/src/main/java/seminario/grupo4/smart_travel/model/dto/DestinoDTO.java
+++ backEnd/src/main/java/seminario/grupo4/smart_travel/model/dto/DestinoDTO.java
@@ -1,2 +1,35 @@
-package seminario.grupo4.smart_travel.model.dto;public class DestinoDTO {
+package seminario.grupo4.smart_travel.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class DestinoDTO {
+    private String ciudadDestino;
+    private Date fechaInicio;
+    private Date fechaFin;
+    private Long viajeId;
+
+    public DestinoDTO() {
+    }
+
+    public DestinoDTO(String ciudadDestino, Date fechaInicio, Date fechaFin, Long viajeId) {
+        this.ciudadDestino = ciudadDestino;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.viajeId = viajeId;
+    }
+
+    @Override
+    public String toString() {
+        return "DestinoDTO{" +
+                "ciudadDestino='" + ciudadDestino + '\'' +
+                ", fechaInicio=" + fechaInicio +
+                ", fechaFin=" + fechaFin +
+                ", viajeId=" + viajeId +
+                '}';
+    }
 }
